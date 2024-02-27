090ad1291483fd358aa92aa57467608b4c481c02
Leonardo Berbin <lberbin.lb@gmail.com>
2024-02-27 07:46:50 -0500

    Establecidas las normas de colision y gravedad y correctamente aplicada. Uso de panResponder para control de salto del player. Resta ajustar detalles en el detector de colisiones

---
+++ app/game/utils/collision.js
@@ -0,0 +1,61 @@
+/* Detecta colisiones entre objeto 
+ * La funcion retornada acepta tres argumentos
+ * array: arreglo de objetos en el mapa
+ * object: objeto colisionable
+ * speed: velocidad de desplazamiento
+ *
+ * Nota: Tanto los obejtos del mapa como el objetos colisionable deben poseer las propiedades position, size tal como se estipulan
+*/
+const base = (a, b, c, d) => {
+   return (array, object, speed) => {
+    // Ajustar condicion segun el eje 
+    const condition = a === 'x' ? (speed > 0) : (speed < 0);
+    
+     // Encuentra el objeto mas cercano
+    const closest = array.filter(e => {
+      if(condition) return (
+        e.position[a] + e.size[b] <= object.position[a]
+        && e.position[c] < object.position[c] + object.size[d]
+        && e.position[c] + e.size[d] > object.position[c]
+      );
+      else return (
+        e.position[a] >= object.position[a] + object.size[b]
+        && e.position[c] < object.position[c] + object.size[d]
+        && e.position[c] + e.size[d] > object.position[c]
+      );
+    })
+    .sort((e, f) => condition ?  f.position[a] - e.position[a] : e.position[a]  - f.position[a])[0];
+    
+    // Retoma el valor preestablecido cuando no hay coincidencias 
+    if(!closest) 
+    return {
+     max: speed,
+     closest
+    };
+    
+    // Calcula las distancias 
+    const distance_1 = Math.abs(closest.position[a] + closest.size[b] - object.position[a]);
+    const distance_2 = Math.abs(object.position[a] + object.size[b] - closest.position[a]);
+  
+    // Obtiene la distancia más coherente
+    const closestDistance = Math.min(distance_1, distance_2)
+
+    // Limita el movimiento al valor mínimo entre la distancia más coherente y la velocidad
+    const movement = Math.min(closestDistance, Math.abs(speed));
+
+    // Devuelve el movimiento limitado
+    return {
+     max: movement * Math.sign(speed),
+     closest
+    };
+  };
+};
+
+/* Exporta un objeto con dos funciones obtenidas de la funcion base
+ * Los argumentos usados permiten reproducir la misma funcion tanto en el eje x como en el eje y
+ * Se asume que x - y, w - h son propiedades validas de position y size respectivamente
+*/
+export default collision = {
+ x: base('x', 'w', 'y', 'h'),
+ y: base('y', 'h', 'x', 'w')
+};
---
+++ app/game/utils/gravity.js
@@ -0,0 +1,54 @@
+/* Simula el efecto gravedad ejerciendo una fuerza de signo negativo. 
+ * Requiere importar collision para sunfuncionamiento
+*/
+import collision from './collision.js';
+
+/* Introduce la propiedad gravity dentro del objeto
+ * array: objetos en el mapa
+ * object: objeto expuesto
+ * jumpForce: fuerza anti gravedad de signo positivo
+*/ 
+const gravity = (array, object, jumpForce=0) => {
+  const g = -0.01;
+  object.gravity = {
+    force: 0, 
+    // Esta funcion debe ser invocada solo una vez en el constructor del objeto, siempre que ya se haya establecido la frecuencia de renderizado
+    init(){
+      // incremento de fuerza y deteccion de colisiones en el eje 'y'
+      this.force += g;
+      const movement = collision.y(array, object, this.force);
+       
+       // Se ejecuta ante colisiones 
+       if(movement.max === 0){
+          // Cuando el objeto va en caida 
+          if(this.force < 0){
+            this.force = 0;
+            // retorna la superficie
+            return surface = movement.closest;
+          }
+          // Cuando el objeto esta ascendiendo
+          else {
+            // Accion - Reaccion
+            this.force -= this.jumpForce;
+            // retorna el obstacull
+            return obstacle = movement.closest;
+          };
+       };
+       // Actualiza la posicion 'y' del objeto 
+       object.position.y += movement.max;
+    },
+    
+    // propiedad anti gravedad 
+    jumpForce: Math.abs(jumpForce),
+    jump(){
+      if(this.force !== 0) return;
+      this.force = this.jumpForce;
+    }
+  };
+};
+
+// exporta el complemento gravity
+export default gravity;
+
+
+
